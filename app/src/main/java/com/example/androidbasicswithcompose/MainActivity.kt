3004db8312c20fc432197c28776b5226ca89f72f
Ozlem Basabakar <ozlembasabakar@gmail.com>
2022-05-13 19:07:00 +0300

    add Compose Quadrant

--- app/src/main/java/com/example/androidbasicswithcompose/MainActivity.kt
+++ app/src/main/java/com/example/androidbasicswithcompose/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.androidbasicswithcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androidbasicswithcompose.ui.theme.AndroidBasicsWithComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ComposeCard()
                 }
             }
         }
@@ -30,14 +36,112 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ComposeCard() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = androidx.compose.ui.graphics.Color.Green)
+                    .padding(16.dp)
+                    .weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(id = R.string.title_up_left),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+
+                )
+                Text(
+                    text = stringResource(id = R.string.text_up_left),
+                    textAlign = TextAlign.Justify
+                )
+            }
+
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = androidx.compose.ui.graphics.Color.Yellow)
+                    .padding(16.dp)
+                    .weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(id = R.string.title_up_right),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+
+                )
+                Text(
+                    text = stringResource(id = R.string.text_up_right),
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+        Row(Modifier.weight(1f)) {
+
+        Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = androidx.compose.ui.graphics.Color.Cyan)
+                    .padding(16.dp)
+                    .weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(id = R.string.title_down_left),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+
+                )
+                Text(
+                    text = stringResource(id = R.string.text_down_left),
+                    textAlign = TextAlign.Justify
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = androidx.compose.ui.graphics.Color.Gray)
+                    .padding(16.dp)
+                    .weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(id = R.string.title_down_right),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+
+                )
+                Text(
+                    text = stringResource(id = R.string.text_down_right),
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
+
+
+
+@Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun DefaultPreview() {
     AndroidBasicsWithComposeTheme {
-        Greeting("Android")
+        ComposeCard()
     }
 }
