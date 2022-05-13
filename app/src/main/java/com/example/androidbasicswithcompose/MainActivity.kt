0aa652cb3517fbb4cb6cf999ae7899002e5f9f38
Ozlem Basabakar <ozlembasabakar@gmail.com>
2022-05-13 16:12:46 +0300

    add text

--- app/src/main/java/com/example/androidbasicswithcompose/MainActivity.kt
+++ app/src/main/java/com/example/androidbasicswithcompose/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.androidbasicswithcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,6 +12,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.androidbasicswithcompose.ui.theme.AndroidBasicsWithComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BirthdayGreetingWithText("Happy Birthday Özlem!!", " - from Google")
                 }
             }
         }
@@ -30,14 +33,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BirthdayGreetingWithText(message: String, from: String) {
+    Column {
+        Text(text = message, fontSize = 36.sp)
+        Text(text = from, fontSize = 24.sp)
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(name = "My Preview", showSystemUi = true)
 @Composable
-fun DefaultPreview() {
+fun DBirthdayGreetingWithTextPreview() {
     AndroidBasicsWithComposeTheme {
-        Greeting("Android")
+        BirthdayGreetingWithText("Happy Birthday Özlem!!", " - from Google")
     }
 }
