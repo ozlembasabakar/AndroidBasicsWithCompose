640446a1e52d225b4a567a0c9df5a8e3d05ab36d
Ozlem Basabakar <ozlembasabakar@gmail.com>
2022-05-13 17:26:22 +0300

    add Compose Article

--- app/src/main/java/com/example/androidbasicswithcompose/MainActivity.kt
+++ app/src/main/java/com/example/androidbasicswithcompose/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.androidbasicswithcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androidbasicswithcompose.ui.theme.AndroidBasicsWithComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ComposeCard()
                 }
             }
         }
@@ -30,14 +37,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ComposeCard() {
+    Column {
+        Image(
+            painter = painterResource(id = R.drawable.bg_compose_background),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(id = R.string.title),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.first_article),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.sec_article),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+
+@Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun ComposeCardPreview() {
     AndroidBasicsWithComposeTheme {
-        Greeting("Android")
+        ComposeCard()
     }
 }
